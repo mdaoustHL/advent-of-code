43bf6acab6a39478553d7db8b9e75d92e8f8eefe
Michel Daoust <mdaoust@honorlock.com>
2021-12-07 13:55:55 -0500

    Tweak 2021 Day 6 PHP solution (not working for second part, OOM)

--- 2021/day6/solve.php
+++ 2021/day6/solve.php
@@ -10,39 +10,10 @@
         return $aquarium;
     }
 
-    require_once "../../common/php/functions.php";
+    function simulate($aquarium, $days) {
+        output("Simulating ${days} days...", 1);
 
-    function main() {
-        welcome(6, 2021, "Lanternfish");
-
-        $aquarium = loadAquarium();
-
-        // Part 1
-        output("Simulating 80 days...", 1);
-
-        for ($day = 1; $day <= 80; $day++) {
-            $new = [];
-            for ($f = 0; $f < sizeof($aquarium); $f++) {
-                $aquarium[$f] -= 1;
-                if ($aquarium[$f] < 0) {
-                    $aquarium[$f] = 6;
-                    array_push($new, 8);
-                }
-            }
-            // Add the newly spawned fish to aquarium
-            $aquarium = array_values(array_merge($aquarium, $new));
-            $count = sizeof($aquarium);
-            output("Day ${day} => ${count}", 1);
-        }
-
-        $part1Result = sizeof($aquarium);
-        output("Part 1 Result: ${part1Result} fish after 80 days.", 2);
-
-        // Part 2
-        $aquarium = loadAquarium(); ini_set("memory_limit", "16000000000"); // 16GB - Doesn't work, script still requires too much memory => OOM error
-        output("Simulating 256 days...", 1);
-
-        for ($day = 1; $day <=256; $day++) {
+        for ($day = 1; $day <= $days; $day++) {
             $currentAquariumSize = sizeof($aquarium);
             for ($f = 0; $f < $currentAquariumSize; $f++) {
                 $aquarium[$f] -= 1;
@@ -52,13 +23,31 @@
                 }
             }
             $count = sizeof($aquarium);
-            output("Day ${day} => ${count}", 1);
+            $diff = $count - $currentAquariumSize;
+            output("Day ${day} => Started at ${currentAquariumSize}, ended at ${count}. Growth of ${diff}", 1);
         }
 
+        return $aquarium;
+    }
+
+    require_once "../../common/php/functions.php";
+
+    function main() {
+        welcome(6, 2021, "Lanternfish");
+
+        // Part 1
+        $aquarium = loadAquarium();
+        $aquarium = simulate($aquarium, 80);
+        $part1Result = sizeof($aquarium);
+        output("Part 1 Result: ${part1Result} fish after 80 days.", 2);
+
+        // Part 2
+        $aquarium = loadAquarium();
+        ini_set("memory_limit", "16000000000"); // 16GB - Doesn't work, script still requires too much memory => OOM error
+        $aquarium = simulate($aquarium, 256);
         $part2Result = sizeof($aquarium);
         output("Part 2 Result: ${part2Result} fish after 256 days.", 2); // If you can get here, congrats! I haven't lol
         // The farthest I've gotten is Day 155 @ 261,566,169 lanterfish before OOM error occurs
-
     }
     main();
 
